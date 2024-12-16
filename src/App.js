fab178d7e71fbb8f3b39d96cc476925fe79293b6
Lee <cookie3013@naver.com>
2024-12-16 17:51:20 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TestMain from "./test/TestMain";
 import PostMappingTest from "./test/PostMappingTest";
 import GoogleLoginTest from "./test/GoogleLoginTest";
+import MainPage from "./page/MainPage";
 
 function App() 
 {
@@ -11,6 +12,7 @@ function App()
                 <Route path="/" element={<TestMain />} />
                 <Route path="/postmappingtest" element={<PostMappingTest />} />
                 <Route path="/oauth2/authorization/google" element={<GoogleLoginTest />} />
+                <Route path="/MainPage" element={<MainPage/>}/>
             </Routes>
         </BrowserRouter>
     );
--- src/test/TestMain.js
+++ src/test/TestMain.js
@@ -13,12 +13,18 @@ const TestMain = () =>
     {
         navigate("/oauth2/authorization/google");
     }
+    const nav3 = () => {
+        navigate("/MainPage");
+    }
+
 
     return (
         <>
             <button onClick={nav}>PostMappingTest</button>
             <br/>
             <button onClick={nav2}>GoogleLoginTest</button>
+            <br/>
+            <button onClick={nav3}>mainpage</button>
 
         </>
     )
