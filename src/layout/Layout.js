7701a10adc9134198eabe6ec8ce2fb80dfdf5a4d
kimsua <k021113a@naver.com>
2024-12-21 21:21:48 +0900

    SmallTimer 크기 조절

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import TopHeader from "../component/TopHeader";
-import SmallTimer from '../page/SmallTimer';
+import SmallTimer from "../pages/SmallTimer";
 
 const Layout = () => {
     return (
         <>
         <TopHeader/>
         <Outlet/>
-        <SmallTimer/>
+        {/*<SmallTimer/>*/}
         </>
     )
 }
