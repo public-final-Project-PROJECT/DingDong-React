896398f47c815bd6c18e322f7249c6f949b896a3
kimsua <k021113a@naver.com>
2025-01-12 20:02:46 +0900

    헤더 색 적용 & 편의 기능 분리 후 css 적용

--- src/component/TopHeader.js
+++ src/component/TopHeader.js
@@ -50,28 +50,29 @@ const TopHeader = () => {
   };
 
   return (
-    <div className="top-header">
-      <div className="main-buttons">
-        <div className="Main" onClick={onClickHandler}>메인</div>
-        <div className="Notice" onClick={onClickHandler}>공지사항</div>
-        <div className="Attendance" onClick={onClickHandler}>출석부</div>
-        <div className="Students" onClick={onClickHandler}>학생정보</div>
-        <div className="Calendar" onClick={onClickHandler}>캘린더</div>
-        <div onClick={toggleFunctions}>편의기능</div>
-        <GoogleOAuthProvider clientId={clientId}>
-          <GoogleLoginButton />
-        </GoogleOAuthProvider>
-      </div>
-
-      {show && (
-        <div className="convenience-buttons">
-          <button className="Timer" onClick={onClickHandler}>타이머</button>
-          <button className="Seat" onClick={onClickHandler}>자리바꾸기</button>
-          <button className="RandomPicker" onClick={onClickHandler}>발표자 뽑기</button>
-          <button className="Voting" onClick={onClickHandler}>학급 투표</button>
+      <div className="top-header">
+        <div className="main-buttons">
+          <div className="Main" onClick={onClickHandler}>메인</div>
+          <div className="Notice" onClick={onClickHandler}>공지사항</div>
+          <div className="Attendance" onClick={onClickHandler}>출석부</div>
+          <div className="Students" onClick={onClickHandler}>학생정보</div>
+          <div className="Calendar" onClick={onClickHandler}>캘린더</div>
+          <div onClick={toggleFunctions}>편의기능</div>
+          <GoogleOAuthProvider clientId={clientId}>
+            <GoogleLoginButton />
+          </GoogleOAuthProvider>
         </div>
-      )}
-    </div>
+
+        {/* 헤더와 분리된 하위 버튼 영역 */}
+        {show && (
+            <div className="convenience-buttons">
+              <button className="Timer" onClick={onClickHandler}>타이머</button>
+              <button className="Seat" onClick={onClickHandler}>자리바꾸기</button>
+              <button className="RandomPicker" onClick={onClickHandler}>발표자 뽑기</button>
+              <button className="Voting" onClick={onClickHandler}>학급 투표</button>
+            </div>
+        )}
+      </div>
   );
 };
 
