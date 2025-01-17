e99a50a48f9d168a5371c928c27b397791d1f44c
RJANG <msgpigeon@gmail.com>
2025-01-17 09:54:07 +0900

    .

--- src/component/TopHeader.js
+++ src/component/TopHeader.js
@@ -6,43 +6,14 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const TopHeader = () => {
   const [show, setShow] = useState(false);
-  const navigator = useNavigate();
+  const navigate = useNavigate();
   const clientId = process.env.REACT_APP_OAUTH2_GOOGLE_CLIENT_ID;
 
   const onClickHandler = (e) => {
     setShow(false);
 
-    switch (e.target.className) {
-      case "Main":
-        navigator("/")
-        break;
-      case "Notice":
-        navigator("/Notice");
-        break;
-      case "Attendance":
-        navigator("/Attendance"); 
-        break;
-      case "Students":
-        navigator("/Students");
-        break;
-      case "Calendar":
-        navigator("/Calendar");
-        break;
-      case "Timer":
-        navigator("/Timer");
-        break;
-      case "Seat":
-        navigator("/Seat");
-        break;
-      case "RandomPicker":
-        navigator("/RandomPicker");
-        break;
-      case "Voting":
-        navigator("/Voting");
-        break;
-      default:
-        break;
-    }
+    if (e.target.className === 'main') navigate("/");
+    else navigate(`/${e.target.className}`);
   };
 
   const toggleFunctions = () => {
@@ -50,29 +21,29 @@ const TopHeader = () => {
   };
 
   return (
-      <div className="top-header">
-        <div className="main-buttons">
-          <div className="Main" onClick={onClickHandler}>메인</div>
-          <div className="Notice" onClick={onClickHandler}>공지사항</div>
-          <div className="Attendance" onClick={onClickHandler}>출석부</div>
-          <div className="Students" onClick={onClickHandler}>학생정보</div>
-          <div className="Calendar" onClick={onClickHandler}>캘린더</div>
-          <div className="Convenience" onClick={toggleFunctions}>편의기능</div>
-          <GoogleOAuthProvider clientId={clientId}>
-            <GoogleLoginButton />
-          </GoogleOAuthProvider>
-        </div>
-
-        {/* 헤더와 분리된 하위 버튼 영역 */}
-        {show && (
-            <div className="convenience-buttons">
-              <button className="Timer" onClick={onClickHandler}>타이머</button>
-              <button className="Seat" onClick={onClickHandler}>자리바꾸기</button>
-              <button className="RandomPicker" onClick={onClickHandler}>발표자 뽑기</button>
-              <button className="Voting" onClick={onClickHandler}>학급 투표</button>
-            </div>
-        )}
+    <div className="top-header">
+      <div className="main-buttons">
+        <div className="main" onClick={onClickHandler}>메인</div>
+        <div className="notice" onClick={onClickHandler}>공지사항</div>
+        <div className="attendance" onClick={onClickHandler}>출석부</div>
+        <div className="students" onClick={onClickHandler}>학생정보</div>
+        <div className="calendar" onClick={onClickHandler}>캘린더</div>
+        <div onClick={toggleFunctions}>편의기능</div>
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLoginButton />
+        </GoogleOAuthProvider>
       </div>
+
+      {show && (
+        <div className="convenience-buttons">
+          <button className="timer" onClick={onClickHandler}>타이머</button>
+          <button className="seat" onClick={onClickHandler}>자리바꾸기</button>
+          <button className="randompicker" onClick={onClickHandler}>발표자 뽑기</button>
+          <button className="voting" onClick={onClickHandler}>학급 투표</button>
+          <button className="drawing" onClick={onClickHandler}>칠판</button>
+        </div>
+      )}
+    </div>
   );
 };
 
